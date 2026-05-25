dc3e0b1025704e43f01bf59b98b9381c08658688
ssh9250 <sh9250r@gmail.com>
2026-05-25 11:38:54 +0900

    add github pages deploy workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ssh9250.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
